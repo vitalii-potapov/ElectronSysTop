9f97451b22710c8a2de86c81dc3671ee5d172908
VitaliiPotapov <vitalii.potapov92@gmail.com>
2020-08-13 16:56:54 +0600

    show notification

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const osu = require('node-os-utils');
 
 const { cpu } = osu;
@@ -5,6 +6,26 @@ const { mem } = osu;
 const { os } = osu;
 
 const cpuOverload = 80;
+const alertFrequency = 5;
+
+function notifyUser() {
+  // eslint-disable-next-line no-new
+  new Notification('CPU Overload', {
+    body: `CPU is over ${cpuOverload}`,
+    icon: path.join(__dirname, 'img', 'icon.png'),
+  });
+}
+
+function runNotify() {
+  const notifyTime = new Date(parseInt(localStorage.getItem('lastNotify') || 0, 10));
+  const now = new Date();
+  const diffTime = Math.abs(now - notifyTime);
+  const minutesPassed = Math.floor(diffTime / (1000 * 60));
+  if (minutesPassed < alertFrequency) return;
+
+  localStorage.setItem('lastNotify', +new Date());
+  notifyUser();
+}
 
 function secondsToDHMS(seconds) {
   const d = Math.floor(seconds / (3600 * 24));
@@ -15,18 +36,23 @@ function secondsToDHMS(seconds) {
 }
 
 async function updateInfo() {
-  const cpuUsage = cpu.usage();
-  const cpuFree = cpu.free();
+  const getInfoCpu = [cpu.usage(), cpu.free()];
+  const cpuUsage = await getInfoCpu[0];
+  const cpuFree = await getInfoCpu[1];
   const cpuProgressEl = document.querySelector('#cpu-progress');
   const cpuUsageEl = document.querySelector('#cpu-usage');
   const cpuFreeEl = document.querySelector('#cpu-free');
   const sysUptimeEl = document.querySelector('#sys-uptime');
 
-  cpuUsageEl.innerText = `${await cpuUsage}%`;
-  cpuProgressEl.style.width = `${await cpuUsage}%`;
-  if (await cpuUsage >= cpuOverload) cpuProgressEl.style.background = 'red';
-  else cpuProgressEl.style.background = '';
-  cpuFreeEl.innerText = `${await cpuFree}%`;
+  cpuUsageEl.innerText = `${cpuUsage}%`;
+  cpuProgressEl.style.width = `${cpuUsage}%`;
+  if (cpuUsage >= cpuOverload) {
+    cpuProgressEl.style.background = 'red';
+    runNotify();
+  } else {
+    cpuProgressEl.style.background = '';
+  }
+  cpuFreeEl.innerText = `${cpuFree}%`;
   sysUptimeEl.innerText = secondsToDHMS(os.uptime());
 }
 updateInfo();
